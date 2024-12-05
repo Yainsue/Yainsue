a9a43ac0139df518f95fd737e6cbc7891d82757a
Yainsue <sueyain.williams@georgebrown.ca>
2024-12-05 13:19:03 -0800

    Update Lab11.sql

--- Lab11.sql
+++ Lab11.sql
@@ -1,3 +1,21 @@
 SELECT title, due_date
+FROM assignments
+WHERE course_id LIKE 'COMP1234'
+
+SELECT min(due_date)
+FROM assignments
+
+SELECT max(due_date)
+FROM asignments
+
+SELECT title, course_id
+FROM assignments
+where due_date = '2024-10-08'
+
+SELECT title, due_date
+FROM assignments
+where due_date = '2024-10%'
+
+SELECT max(due_date)
 from assignments
-where course_id LIKE 'COMP1234'
+WHERE status LIKE 'complete'
